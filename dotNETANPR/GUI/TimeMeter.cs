f06e219c0b42574d863adc5320bd7f6b0242506f
bolorundurowb <ogatimo@gmail.com>
2016-12-18 18:28:00 +0100

    feature: implement time meter

---
+++ dotNETANPR/GUI/TimeMeter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dotNETANPR.GUI
+{
+    public class TimeMeter
+    {
+        private long startTime;
+
+        public TimeMeter()
+        {
+            this.startTime = (long) DateTime.Now.TimeOfDay.TotalMilliseconds;
+        }
+        public long GetTime()
+        {
+            return (long) (DateTime.Now.TimeOfDay.TotalMilliseconds - this.startTime);
+        }
+    }
+}

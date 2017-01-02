6827b698d89c2f7094059bd29f69c9b5fd64109a
bolorundurowb <ogatimo@gmail.com>
2017-01-03 00:21:53 +0100

    feature: completes implementation of the character recognizer

--- dotNETANPR/Recognizer/CharacterRecognizer.cs
+++ dotNETANPR/Recognizer/CharacterRecognizer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace dotNETANPR.Recognizer
 {
@@ -41,7 +43,7 @@ namespace dotNETANPR.Recognizer
                 }
             }
 
-            public class PatternComparer : IComparable
+            public class PatternComparer : IComparer<RecognizedPattern>
             {
                 public int Direction { get; set; }
 
@@ -50,12 +52,108 @@ namespace dotNETANPR.Recognizer
                     Direction = direction;
                 }
 
-                public int CompareTo(object obj)
+                public int Compare(RecognizedPattern pattern1, RecognizedPattern pattern2)
                 {
-//                    float cost1 = ((RecognizedPattern) this).Cost;
-                    float cost2 = ((RecognizedPattern) obj).Cost;
-                    return -1;
+                    float cost1 = pattern1.Cost;
+                    float cost2 = pattern1.Cost;
+                    int ret = 0;
+                    if (cost1 < cost2)
+                    {
+                        ret = -1;
+                    }
+                    if (cost1 > cost2)
+                    {
+                        ret = 1;
+                    }
+                    if (Direction == 1)
+                    {
+                        ret *= -1;
+                    }
+                    return ret;
                 }
+            }
+
+            private List<RecognizedPattern> _patterns;
+            public List<RecognizedPattern> Patterns
+            {
+                get
+                {
+                    if (IsSorted)
+                    {
+                        return _patterns;
+                    }
+                    return null;
+                }
+                set { _patterns = value; }
+            }
+            public bool IsSorted { get; set; }
+
+            public RecognizedChar()
+            {
+                Patterns = new List<RecognizedPattern>();
+                IsSorted = false;
+            }
+
+            public void AddPattern(RecognizedPattern pattern)
+            {
+                Patterns.Add(pattern);
+            }
+
+            public void Sort(int direction)
+            {
+                if (IsSorted)
+                {
+                    return;
+                }
+                IsSorted = true;
+                _patterns.Sort(new PatternComparer(direction));
+            }
+
+            public RecognizedPattern GetPattern(int index)
+            {
+                return _patterns[index];
+            }
+
+            public Bitmap Render()
+            {
+                int width = 500;
+                int height = 200;
+                Bitmap histogram = new Bitmap(width + 20, height + 20, PixelFormat.Format8bppIndexed);
+                Graphics graphic = Graphics.FromImage(histogram);
+
+                Pen graphicPen = new Pen(Color.LightGray);
+                SolidBrush graphicBrush = new SolidBrush(Color.LightGray);
+                Rectangle backRect = new Rectangle(0, 0, width + 20, height + 20);
+                graphic.FillRectangle(graphicBrush, backRect);
+                graphic.DrawRectangle(graphicPen, backRect);
+
+                graphicPen.Color = Color.Black;
+                graphicBrush.Color = Color.Black;
+                Font graphicFont = new Font("Consolas", 20);
+
+                int colWidth = width / _patterns.Count;
+                int left, top;
+
+
+                for (int i = 0; i <= 100; i += 10)
+                {
+                    int y = 15 + (int) ((100 - i) / 100.0f * (height - 20));
+                    graphic.DrawString(i.ToString(), graphicFont, graphicBrush, 3, y + 11);
+                    graphic.DrawLine(graphicPen, 25, y + 5, 35, y + 5);
+                }
+                graphic.DrawLine(graphicPen, 35, 19, 35, height);
+
+                graphicPen.Color = Color.Blue;
+                graphicBrush.Color = Color.Blue;
+
+                for (int i = 0; i < _patterns.Count; i++)
+                {
+                    left = i * colWidth + 42;
+                    top = height - (int) (_patterns[i].Cost * (height - 20));
+                    graphic.DrawRectangle(graphicPen, left, top, colWidth - 2, height - top);
+                    graphic.DrawString(_patterns[i].Character + " ", graphicFont, graphicBrush, left + 2, top - 8);
+                }
+                return histogram;
             }
         }
     }
---
+++ dotNETANPR/Recognizer/ICharacterRecognizer.cs
@@ -0,0 +1,9 @@
+﻿using dotNETANPR.ImageAnalysis;
+
+namespace dotNETANPR.Recognizer
+{
+    public interface ICharacterRecognizer
+    {
+        CharacterRecognizer.RecognizedChar Recognize(Character character);
+    }
+}

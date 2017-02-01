13b01800706514bf1ad68f2eaf4b25c09b911a03
bolorundurowb <ogatimo@gmail.com>
2017-02-01 12:16:52 +0100

    feature: implements the piece class

--- dotNETANPR/ImageAnalysis/PixelMap.cs
+++ dotNETANPR/ImageAnalysis/PixelMap.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using dotNETANPR.ImageAnalysis.Util;
 
 namespace dotNETANPR.ImageAnalysis
 {
     public class PixelMap
     {
         private Piece bestPiece = null;
+        private int height;
+        private int width;
+        public static bool[,] matrix;
+
         public class Point
         {
             public int X { get; set; }
@@ -28,13 +36,11 @@ namespace dotNETANPR.ImageAnalysis
 
             public bool Value()
             {
-                //TODO: replace hardcoded value with evaluated value
-//                return matrix[X, Y];
-                return true;
+                return matrix[X, Y];
             }
         }
 
-        public class PointSet : Stack<Point>
+        public class PointSet : CustomStack<Point>
         {
             public static long SerialVersionuid = 0;
 
@@ -48,7 +54,7 @@ namespace dotNETANPR.ImageAnalysis
                         toRemove = px;
                     }
                 }
-                RemovePoint(toRemove);
+                Remove(toRemove);
             }
         }
 
@@ -59,7 +65,148 @@ namespace dotNETANPR.ImageAnalysis
         
         public class Piece : PointSet
         {
-            
+            public static long SerialVersionuid = 0;
+            private int mostLeftPoint;
+            private int mostRightPoint;
+            private int mostTopPoint;
+            private int mostBottomPoint;
+            private int width;
+            private int height;
+            private int centerX;
+            private int centerY;
+            private float magnitude;
+            private int numberOfBlackPoints;
+            private int numberOfAllPoints;
+
+            public Bitmap Render()
+            {
+                if (numberOfAllPoints == 0)
+                {
+                    return null;
+                }
+                Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+                for (int x = mostLeftPoint; x < mostRightPoint; x++)
+                {
+                    for (int y = mostTopPoint; y < mostBottomPoint; y++)
+                    {
+                        if (matrix[x, y])
+                        {
+                            bitmap.SetPixel(x - mostLeftPoint,
+                                y - mostTopPoint,
+                                Color.Black);
+                        }
+                        else
+                        {
+                            bitmap.SetPixel(x - mostLeftPoint,
+                                y - mostTopPoint,
+                                Color.White);
+                        }
+                    }
+                }
+                return bitmap;
+            }
+
+            public void CreateStatistics()
+            {
+                mostLeftPoint = MostLeftPoint();
+                mostRightPoint = MostRightPoint();
+                mostTopPoint = MostTopPoint();
+                mostBottomPoint = MostBottomPoint();
+                width = mostRightPoint - mostLeftPoint + 1;
+                height = mostBottomPoint - mostTopPoint + 1;
+                centerX = (mostLeftPoint + mostRightPoint) / 2;
+                centerY = (mostTopPoint + mostBottomPoint) / 2;
+                numberOfBlackPoints = NumberOfBlackPoints();
+                numberOfAllPoints = NumberOfAllPoints();
+                magnitude = Magnitude();
+            }
+
+            public int Cost()
+            {
+                return numberOfAllPoints - NumberOfBlackPoints();
+            }
+
+            public void BleachPiece()
+            {
+                foreach (Point point in this)
+                {
+                    matrix[point.X, point.Y] = false;
+                }
+            }
+
+            private float Magnitude()
+            {
+                return (float) numberOfBlackPoints / numberOfAllPoints;
+            }
+
+            private int NumberOfAllPoints()
+            {
+                return width * height;
+            }
+
+            private int NumberOfBlackPoints()
+            {
+                return Count;
+            }
+
+            private int MostRightPoint()
+            {
+                int position = 0;
+                foreach (Point point in this)
+                {
+                    position = Math.Max(position, point.X);
+                }
+                return position;
+            }
+
+            private int MostLeftPoint()
+            {
+                int position = Int32.MaxValue;
+                foreach (Point point in this)
+                {
+                    position = Math.Min(position, point.X);
+                }
+                return position;
+            }
+
+            private int MostBottomPoint()
+            {
+                int position = 0;
+                foreach (Point point in this)
+                {
+                    position = Math.Max(position, point.Y);
+                }
+                return position;
+            }
+
+            private int MostTopPoint()
+            {
+                int position = Int32.MaxValue;
+                foreach (Point point in this)
+                {
+                    position = Math.Min(position, point.Y);
+                }
+                return position;
+            }
+        }
+
+        public PixelMap(Photo photo)
+        {
+            MatrixInit(photo);
+        }
+
+        public void MatrixInit(Photo photo)
+        {
+            width = photo.GetWidth();
+            height = photo.GetHeight();
+            matrix = new bool[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    matrix[x, y] = photo.GetBrightness(x, y) < 0.5;
+                }
+            }
         }
     }
 }

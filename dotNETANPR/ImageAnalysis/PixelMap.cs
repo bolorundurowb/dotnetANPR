3a5417ab2902c41dd73029eb5923d77c06b17f7c
bolorundurowb <ogatimo@gmail.com>
2017-02-02 18:44:38 +0100

    feature: implements a few new functions including skeletonize

--- dotNETANPR/ImageAnalysis/PixelMap.cs
+++ dotNETANPR/ImageAnalysis/PixelMap.cs
@@ -208,5 +208,177 @@ namespace dotNETANPR.ImageAnalysis
                 }
             }
         }
+
+        public Bitmap Render()
+        {
+            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (matrix[x, y])
+                    {
+                        bitmap.SetPixel(x, y, Color.Black);
+                    }
+                    else
+                    {
+                        bitmap.SetPixel(x, y, Color.White);
+                    }
+                }
+            }
+            return bitmap;
+        }
+
+        public Piece GetBestPiece()
+        {
+            ReduceOtherPieces();
+            if (bestPiece == null)
+            {
+                return new Piece();
+            }
+            return bestPiece;
+        }
+
+        private bool GetPointValue(int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= width || y >= height)
+            {
+                return false;
+            }
+            return matrix[x, y];
+        }
+
+        private bool IsBoundaryPoint(int x, int y)
+        {
+            if (!GetPointValue(x, y))
+            {
+                return false;
+            }
+
+            if (!GetPointValue(x - 1, y - 1) ||
+                !GetPointValue(x - 1, y + 1) ||
+                !GetPointValue(x + 1, y - 1) ||
+                !GetPointValue(x + 1, y + 1) ||
+                !GetPointValue(x, y + 1) ||
+                !GetPointValue(x, y - 1) ||
+                !GetPointValue(x + 1, y) ||
+                !GetPointValue(x - 1, y))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private int N(int x, int y)
+        {
+            int n = 0;
+            if (GetPointValue(x - 1, y - 1)) n++;
+            if (GetPointValue(x - 1, y + 1)) n++;
+            if (GetPointValue(x + 1, y - 1)) n++;
+            if (GetPointValue(x + 1, y + 1)) n++;
+            if (GetPointValue(x, y + 1)) n++;
+            if (GetPointValue(x, y - 1)) n++;
+            if (GetPointValue(x + 1, y)) n++;
+            if (GetPointValue(x - 1, y)) n++;
+            return n;
+        }
+
+        private int T(int x, int y)
+        {
+            int n = 0;
+            for (int i = 2; i <= 8; i++)
+            {
+                if (!P(i, x, y) && P(i + 1, x, y)) n++;
+            }
+            if (!P(9, x, y) && P(2, x, y)) n++;
+            return n;
+        }
+
+        private bool P(int i, int x, int y)
+        {
+            if (i == 1) return GetPointValue(x, y);
+            if (i == 2) return GetPointValue(x, y - 1);
+            if (i == 3) return GetPointValue(x + 1, y - 1);
+            if (i == 4) return GetPointValue(x + 1, y);
+            if (i == 5) return GetPointValue(x + 1, y + 1);
+            if (i == 6) return GetPointValue(x, y + 1);
+            if (i == 7) return GetPointValue(x - 1, y + 1);
+            if (i == 8) return GetPointValue(x - 1, y);
+            if (i == 9) return GetPointValue(x - 1, y - 1);
+            return false;
+        }
+
+        private bool Step1Passed(int x, int y)
+        {
+            int n = N(x, y);
+            return 2 <= n && n <= 6 &&
+                   T(x, y) == 1 &&
+                   (!P(2, x, y) || !P(4, x, y) || !P(6, x, y)) &&
+                   (!P(4, x, y) || !P(6, x, y) || !P(8, x, y));
+        }
+
+        private bool Step2Passed(int x, int y)
+        {
+            int n = N(x, y);
+            return 2 <= n && n <= 6 &&
+                   T(x, y) == 1 &&
+                   (!P(2, x, y) || !P(4, x, y) || !P(8, x, y)) &&
+                   (!P(2, x, y) || !P(6, x, y) || !P(8, x, y));
+        }
+
+        private void FindBoundaryPoints(PointSet set)
+        {
+            if (set.Count != 0) set.Clear();
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (IsBoundaryPoint(x, y)) set.Add(new Point(x, y));
+                }
+            }
+        }
+
+        public PixelMap Skeletonize()
+        {
+            PointSet flaggedPoints = new PointSet();
+            PointSet boundaryPoints = new PointSet();
+            bool cont;
+
+            do
+            {
+                cont = false;
+                FindBoundaryPoints(boundaryPoints);
+
+                foreach (Point p in boundaryPoints)
+                {
+                    if (Step1Passed(p.X, p.Y))
+                        flaggedPoints.Add(p);
+                }
+
+                if (flaggedPoints.Count != 0)
+                    cont = true;
+                foreach (Point p in flaggedPoints)
+                {
+                    matrix[p.X, p.Y] = false;
+                    boundaryPoints.Remove(p);
+                }
+                flaggedPoints.Clear();
+
+                foreach (Point p in boundaryPoints)
+                {
+                    if (Step2Passed(p.X, p.Y))
+                        flaggedPoints.Add(p);
+                }
+
+                if (flaggedPoints.Count != 0) cont = true;
+                foreach (Point p in flaggedPoints)
+                {
+                    matrix[p.X, p.Y] = false;
+                }
+                boundaryPoints.Clear();
+                flaggedPoints.Clear();
+            } while (cont);
+            return this;
+        }
     }
 }

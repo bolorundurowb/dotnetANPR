04b3966c01c24e978bd1e9846e6707181337ca0f
Bolorunduro Winner-Timothy Bamidele <ogatimo@gmail.com>
2017-03-18 12:45:15 +0100

    feature(carsnapshotgraph): implements the peakcomparer class

--- dotNETANPR/ImageAnalysis/CarSnapshotGraph.cs
+++ dotNETANPR/ImageAnalysis/CarSnapshotGraph.cs
@@ -1,10 +1,43 @@
-﻿﻿namespace dotNETANPR.ImageAnalysis
+﻿﻿using System.Collections.Generic;
+
+namespace dotNETANPR.ImageAnalysis
 {
     public class CarSnapshotGraph : Graph
     {
+        private static double peakFootConstant=
+            Intelligence.Configurator.GetDoubleProperty("carsnapshotgraph_peakfootconstant");
+        private static double peakDiffMultiplicationConstant =
+            Intelligence.Configurator.GetDoubleProperty("carsnapshotgraph_peakDiffMultiplicationConstant");
+        CarSnapshot handle;
+
         public CarSnapshotGraph(CarSnapshot carSnapshot)
         {
+            this.handle = carSnapshot;
+        }
 
+        public class PeakComparer : IComparer<Peak>
+        {
+            private List<float> yValues = null;
+
+            public PeakComparer(List<float> yValues)
+            {
+                this.yValues = yValues;
+            }
+
+            private float GetPeakValue(Peak peak)
+            {
+                return yValues[peak.Center];
+            }
+
+            public int Compare(Peak x, Peak y)
+            {
+                double comparison = GetPeakValue(y) - GetPeakValue(x);
+                if (comparison < 0)
+                {
+                    return -1;
+                }
+                return comparison > 0 ? 1 : 0;
+            }
         }
 
         public void FindPeaks(int numberOfCandidates)

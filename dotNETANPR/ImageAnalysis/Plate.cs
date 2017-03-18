eb2140ab7a24b8349e5e5bbe2920ee1fff7d5ece
Bolorunduro Winner-Timothy Bamidele <ogatimo@gmail.com>
2017-03-18 13:25:06 +0100

    feature(plate): initial work

--- dotNETANPR/ImageAnalysis/Plate.cs
+++ dotNETANPR/ImageAnalysis/Plate.cs
@@ -4,9 +4,28 @@ namespace dotNETANPR.ImageAnalysis
 {
     public class Plate : Photo
     {
-        public Plate(Bitmap bitmap)
-        {
+        public static Graph.ProbabilityDistributor distributor = new Graph.ProbabilityDistributor(0, 0, 0, 0);
 
+        private static int numberOfCandidates =
+            Intelligence.Intelligence.Configurator.GetIntProperty("intelligence_numberOfImageAnalysis.Chars");
+
+        private static int horizontalDetectionType =
+            Intelligence.Intelligence.Configurator.GetIntProperty("platehorizontalgraph_detectionType");
+
+        private PlateGraph graphHandle = null;
+        public Plate plateCopy;
+
+        public Plate()
+        {
+            this.Image = null;
         }
+
+        public Plate(Bitmap bitmap) : base(bitmap)
+        {
+            plateCopy = new Plate(DuplicateBitmap(this.Image), true);
+            plateCopy.AdaptiveThresholding();
+        }
+
+        public Plate(Bitmap bitmap, bool isCopy) : base(bitmap) {}
     }
 }

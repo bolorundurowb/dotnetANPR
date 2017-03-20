abb8a960a3243a2e9ede99786ac7ce7d9c0f0ecf
Bolorunduro Winner-Timothy Bamidele <ogatimo@gmail.com>
2017-03-21 00:34:10 +0100

    feature(plate): imports and refactors previous implementation

--- dotNETANPR/ImageAnalysis/Character.cs
+++ dotNETANPR/ImageAnalysis/Character.cs
@@ -1,7 +1,23 @@
-﻿namespace dotNETANPR.ImageAnalysis
+﻿using System.Drawing;
+
+namespace dotNETANPR.ImageAnalysis
 {
     public class Character
     {
+        public Character(Bitmap clone, Bitmap bitmap, PositionInPlate positionInPlate)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public float FullWidth { get; set; }
+        public float PieceWidth { get; set; }
+        public float StatisticAverageHue { get; set; }
+        public float StatisticContrast { get; set; }
+        public float StatisticAverageBrightness { get; set; }
+        public float StatisticMinimumBrightness { get; set; }
+        public float StatisticMaximumBrightness { get; set; }
+        public float StatisticAverageSaturation { get; set; }
+        public float FullHeight { get; set; }
+        public float PieceHeight { get; set; }
     }
 }

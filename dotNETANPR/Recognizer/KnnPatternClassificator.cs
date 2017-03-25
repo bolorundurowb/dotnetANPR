97d7c94327866dce12ee6d7f4f623182c65726e9
bolorundurowb <ogatimo@gmail.com>
2017-03-25 12:38:48 +0100

    chore(pattern): some syntax improvements

--- dotNETANPR/Recognizer/KnnPatternClassificator.cs
+++ dotNETANPR/Recognizer/KnnPatternClassificator.cs
@@ -8,14 +8,14 @@ namespace dotNETANPR.Recognizer
 {
     public class KnnPatterClassificator : CharacterRecognizer
     {
-        private readonly List<List<double>> learnLists;
+        private readonly List<List<double>> _learnLists;
 
         public KnnPatterClassificator()
         {
             string path = Intelligence.Intelligence.Configurator.GetPathProperty("char_learnAlphabetPath");
             string alphastring = "0123456789abcdefghijklmnopqrstuvwxyz";
-            learnLists = new List<List<double>>();
-            for (int i = 0; i < alphastring.Length; i++) learnLists.Add(null);
+            _learnLists = new List<List<double>>();
+            for (int i = 0; i < alphastring.Length; i++) _learnLists.Add(null);
 
 
             foreach (string fileName in Directory.GetFiles(path))
@@ -25,11 +25,11 @@ namespace dotNETANPR.Recognizer
 
                 Character imgChar = new Character(path + Path.DirectorySeparatorChar + fileName);
                 imgChar.Normalize();
-                learnLists.Insert(alphaPosition, imgChar.ExtractFeatures());
+                _learnLists.Insert(alphaPosition, imgChar.ExtractFeatures());
             }
 
             for (int i = 0; i < alphastring.Length; i++)
-                if (learnLists.ElementAt(i) == null)
+                if (_learnLists.ElementAt(i) == null)
                     throw new IOException("Warning : alphabet in " + path + " is not complete");
         }
         
@@ -39,9 +39,9 @@ namespace dotNETANPR.Recognizer
             int minx = 0;
             float minfx = float.PositiveInfinity;
             RecognizedChar recognized = new RecognizedChar();
-            for (int x = 0; x < learnLists.Count; x++)
+            for (int x = 0; x < _learnLists.Count; x++)
             {
-                float fx = SimplifiedEuclideanDistance(tested, learnLists.ElementAt(x));
+                float fx = SimplifiedEuclideanDistance(tested, _learnLists.ElementAt(x));
                 recognized.AddPattern(new RecognizedChar.RecognizedPattern(alphabet[x], fx));
             }
             recognized.Sort(0);
@@ -50,23 +50,12 @@ namespace dotNETANPR.Recognizer
 
         public float Difference(List<double> vectorA, List<double> vectorB)
         {
-            float diff = 0;
-            for (int x = 0; x < vectorA.Count; x++)
-            {
-                diff += Math.Abs((float)(vectorA.ElementAt(x) - vectorB.ElementAt(x)));
-            }
-            return diff;
+            return vectorA.Select((t, x) => Math.Abs((float) (vectorA.ElementAt(x) - vectorB.ElementAt(x)))).Sum();
         }
 
         public float SimplifiedEuclideanDistance(List<double> vectorA, List<double> vectorB)
         {
-            float diff = 0;
-            for (int x = 0; x < vectorA.Count; x++)
-            {
-                var partialDiff = Math.Abs((float)(vectorA[x] - vectorB[x]));
-                diff += partialDiff * partialDiff;
-            }
-            return diff;
+            return vectorA.Select((t, x) => Math.Abs((float) (t - vectorB[x]))).Sum(partialDiff => partialDiff * partialDiff);
         }
     }
 }

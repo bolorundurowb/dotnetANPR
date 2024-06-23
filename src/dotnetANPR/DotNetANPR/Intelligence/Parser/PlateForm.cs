618449721cb8cd13c4aaad0428eb48a19d766561
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2024-06-24 00:23:38 +0100

    ft: add in the plate form class

---
+++ src/dotnetANPR/DotNetANPR/Intelligence/Parser/PlateForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetANPR.Intelligence.Parser
+{
+    public class PlateForm
+    {
+        public List<Position> Positions { get; private set; }
+
+        public bool IsFlagged { get; set; }
+
+        public string Name { get; set; }
+
+        public PlateForm(string name)
+        {
+            this.Name = name;
+            this.Positions = new List<Position>();
+        }
+    }
+}

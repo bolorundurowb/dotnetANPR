1ccce6130b4651fa7eb145da0f38904d3f72424b
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2024-06-24 00:02:46 +0100

    ft: add in the peak class

---
+++ src/dotnetANPR/DotNetANPR/ImageAnalysis/Peak.cs
@@ -0,0 +1,27 @@
+﻿namespace DotNetANPR.ImageAnalysis
+{
+    public class Peak
+    {
+        public int Left { get; set; }
+
+        public int Center { get; set; }
+
+        public int Right { get; set; }
+
+        public int Diff => Right - Left;
+
+        public Peak(int left, int right)
+        {
+            Left = left;
+            Center = (left + right) / 2;
+            Right = right;
+        }
+
+        public Peak(int left, int center, int right)
+        {
+            Left = left;
+            Center = center;
+            Right = right;
+        }
+    }
+}

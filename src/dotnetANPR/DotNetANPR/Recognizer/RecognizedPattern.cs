13a3f7ad6356b6cf0adc9ed5c84bb4283dccdf04
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2024-06-23 22:58:51 +0100

    ft: add in the recognized pattern class

---
+++ src/dotnetANPR/DotNetANPR/Recognizer/RecognizedPattern.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNetANPR.Recognizer
+{
+    public class RecognizedPattern
+    {
+        public char Char { get; private set; }
+
+        public double Cost { get; private set; }
+
+        public RecognizedPattern(char c, double cost)
+        {
+            Char = c;
+            Cost = cost;
+        }
+    }
+}

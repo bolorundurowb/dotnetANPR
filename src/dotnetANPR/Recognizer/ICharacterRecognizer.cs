c16495af7fc463f349f497a11952992c66605bb3
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2024-06-23 22:34:16 +0100

    ft: remove old code in preparation for new approach

--- src/dotnetANPR/Recognizer/ICharacterRecognizer.cs
+++
@@ -1,9 +0,0 @@
-﻿using dotnetANPR.ImageAnalysis;
-
-namespace dotnetANPR.Recognizer
-{
-    public interface ICharacterRecognizer
-    {
-        CharacterRecognizer.RecognizedChar Recognize(Character character);
-    }
-}

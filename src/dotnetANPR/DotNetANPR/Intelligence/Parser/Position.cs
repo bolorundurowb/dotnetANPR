1c0b775d6c9375a5efa11a8aa5fae33f8e738a73
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2024-06-24 00:15:54 +0100

    ft: add in the position class

---
+++ src/dotnetANPR/DotNetANPR/Intelligence/Parser/Position.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace DotNetANPR.Intelligence.Parser
+{
+    public class Position
+    {
+        public char[] AllowedChars { get; private set; }
+
+        public Position(string data) => AllowedChars = data.ToCharArray();
+
+        public bool IsAllowed(char character) => AllowedChars.Contains(character);
+    }
+}

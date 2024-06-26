dc8c223b4ceea7ef0f1d3f1be33de1405fe352a1
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-06-26 21:36:50 +0100

    ft: add in the recognized plate

---
+++ src/dotnetANPR/DotNetANPR/Intelligence/RecognizedPlate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using DotNetANPR.Recognizer;
+
+namespace DotNetANPR.Intelligence
+{
+    public class RecognizedPlate
+    {
+        private readonly List<RecognizedCharacter> _characters = new List<RecognizedCharacter>();
+
+        public void AddCharacter(RecognizedCharacter character) => _characters.Add(character);
+
+        public RecognizedCharacter GetCharacter(int index) => _characters[index];
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            foreach (var character in _characters)
+                builder.Append(character.Pattern.Character);
+
+            return builder.ToString();
+        }
+    }
+}

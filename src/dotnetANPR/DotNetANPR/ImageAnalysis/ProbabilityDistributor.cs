aea66ba439e0b2b7b0fb86a360f360a204fd0c74
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-06-28 10:39:42 +0100

    ft: add in the probability distributor class

---
+++ src/dotnetANPR/DotNetANPR/ImageAnalysis/ProbabilityDistributor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetANPR.ImageAnalysis;
+
+public class ProbabilityDistributor(float center, float power, int leftMargin, int rightMargin)
+{
+    private readonly int _leftMargin = Math.Max(1, leftMargin);
+    private readonly int _rightMargin = Math.Max(1, rightMargin);
+
+    public List<float> Distribute(List<float> peaks)
+    {
+        var distributedPeaks = new List<float>();
+
+        for (var i = 0; i < peaks.Count; i++)
+        {
+            if ((i < _leftMargin) || (i > (peaks.Count - _rightMargin)))
+                distributedPeaks.Add(0f);
+            else
+                distributedPeaks.Add(DistributionFunction(peaks[i], ((float)i / peaks.Count)));
+        }
+
+        return distributedPeaks;
+    }
+
+    private float DistributionFunction(float value, float positionPercentage) =>
+        value * (1 - (power * Math.Abs(positionPercentage - center)));
+}

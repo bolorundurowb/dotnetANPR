e168a250f093b6ba41b8bd338a2fd6553244b8c9
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-07-18 13:36:08 +0100

    ft: add pixel map scaffold

---
+++ src/dotnetANPR/DotNetANPR/ImageAnalysis/PixelMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace DotNetANPR.ImageAnalysis;
+
+public class PixelMap(Character character)
+{
+    public class Piece
+    {
+        public Bitmap Render() { throw new System.NotImplementedException(); }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int MostLeftPoint() { throw new System.NotImplementedException(); }
+        public int MostTopPoint() { throw new System.NotImplementedException(); }
+    }
+
+    public Piece BestPiece() { throw new System.NotImplementedException(); }
+}

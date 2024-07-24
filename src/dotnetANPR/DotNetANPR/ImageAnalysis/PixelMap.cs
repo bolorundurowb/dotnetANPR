b0e8b9402134c2cbad0f5533488b691ff7121c7d
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-07-24 23:12:20 +0100

    ft: add in pixel map point class

--- src/dotnetANPR/DotNetANPR/ImageAnalysis/PixelMap.cs
+++ src/dotnetANPR/DotNetANPR/ImageAnalysis/PixelMap.cs
@@ -1,9 +1,29 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace DotNetANPR.ImageAnalysis;
 
-public class PixelMap(Character character)
+public class PixelMap(Photo photo)
 {
+    public Piece BestPiece() { throw new System.NotImplementedException(); }
+
+    private sealed class Point(int x, int y) : IEquatable<Point>
+    {
+        public int X { get; private set; } = x;
+        
+        public int Y { get; private set; } = y;
+
+        public bool Equals(Point? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return X == other.X && Y == other.Y;
+        }
+
+        public static bool operator ==(Point? left, Point? right) => Equals(left, right);
+        public static bool operator !=(Point? left, Point? right) => !Equals(left, right);
+    }
+    
     public class Piece
     {
         public Bitmap Render() { throw new System.NotImplementedException(); }
@@ -16,6 +36,4 @@ public class PixelMap(Character character)
 
         public int MostTopPoint() { throw new System.NotImplementedException(); }
     }
-
-    public Piece BestPiece() { throw new System.NotImplementedException(); }
 }

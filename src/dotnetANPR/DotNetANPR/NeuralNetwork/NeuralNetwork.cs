b349b9ef2106480edc4e795ed8b7516715c37db1
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-07-26 14:52:35 +0100

    ft: add the neural network class

---
+++ src/dotnetANPR/DotNetANPR/NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetANPR.NeuralNetwork;
+
+public class NeuralNetwork
+{
+    public NeuralNetwork(List<int> dimensions) { }
+
+    public NeuralNetwork(string path) { }
+
+    public static List<double> Test(List<double> extractFeatures) { throw new System.NotImplementedException(); }
+
+    public void Learn(SetOfIOPairs train, int get, double d, double get1, double d1)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public class SetOfIOPairs
+    {
+        public void AddIOPair(IOPair createNewPair) { throw new System.NotImplementedException(); }
+
+        public class IOPair
+        {
+            public IOPair(List<double> vectorInput, List<double> vectorOutput)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}

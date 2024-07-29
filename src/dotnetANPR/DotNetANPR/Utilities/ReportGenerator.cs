5dda2be7ea16f30089d6eba845df45591eb7b738
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-07-29 04:10:14 +0100

    ft: clean the report generator up

--- src/dotnetANPR/DotNetANPR/Utilities/ReportGenerator.cs
+++ src/dotnetANPR/DotNetANPR/Utilities/ReportGenerator.cs
@@ -9,67 +9,87 @@ namespace DotNetANPR.Utilities;
 
 public class ReportGenerator
 {
-    private static readonly HashSet<string> supportedFileFormats = new HashSet<string>()
-    {
+    private static readonly HashSet<string> SupportedImageFormats =
+    [
         "bmp",
         "jpg",
         "jpeg",
         "png"
-    };
+    ];
 
-    private String directory;
-    private StringBuilder output; // TODO refactor into a form
+    private readonly string _reportDirectory;
+    private readonly StringBuilder _reportContent;
 
-    public ReportGenerator(String directory)
+    /// <summary>
+    /// Initializes a new instance of the ReportGenerator class.
+    /// </summary>
+    /// <param name="directory">The directory where the report will be saved.</param>
+    public ReportGenerator(string directory)
     {
-        this.directory = directory;
+        _reportDirectory = directory;
 
         if (!Directory.Exists(directory))
             Directory.CreateDirectory(directory);
 
-        output = new StringBuilder();
-        output.Append("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">" + "<html>"
+        _reportContent = new StringBuilder();
+        _reportContent.Append("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">" + "<html>"
             + "<head><title>ANPR report</title>" + "</head>" + "<style type=\"text/css\">"
             + "@import \"style.css\";" + "</style>");
     }
 
-    public void InsertText(String text) => output.AppendLine(text);
+    /// <summary>
+    /// Inserts text into the report.
+    /// </summary>
+    /// <param name="text">The text to be inserted.</param>
+    public void InsertText(string text) => _reportContent.AppendLine(text);
 
-    public void InsertImage(Bitmap image, String cls, int w, int h)
+    /// <summary>
+    /// Inserts an image into the report.
+    /// </summary>
+    /// <param name="image">The image to be inserted.</param>
+    /// <param name="cssClass">The CSS class to be applied to the image.</param>
+    /// <param name="width">The width of the image.</param>
+    /// <param name="height">The height of the image.</param>
+    public void InsertImage(Bitmap image, string cssClass, int width, int height)
     {
-        String imageName = image.GetHashCode() + ".jpg";
+        var imageName = image.GetHashCode() + ".jpg";
         SaveImage(image, imageName);
-        if ((w != 0) && (h != 0))
-        {
-            output.Append("<img src='").Append(imageName).Append("' alt='' width='").Append(w).Append("' height='")
-                .Append(h).Append("' class='").Append(cls).AppendLine("'>");
-        }
+        if ((width != 0) && (height != 0))
+            _reportContent.Append("<img src='").Append(imageName).Append("' alt='' width='").Append(width)
+                .Append("' height='")
+                .Append(height).Append("' class='").Append(cssClass).AppendLine("'>");
         else
-        {
-            output.Append("<img src='").Append(imageName).Append("' alt='' class='").Append(cls).AppendLine("'>");
-        }
+            _reportContent.Append("<img src='").Append(imageName).Append("' alt='' class='").Append(cssClass)
+                .AppendLine("'>");
     }
 
+    /// <summary>
+    /// Finalizes the report by appending the closing HTML tag and saving the report content.
+    /// </summary>
     public void Finish()
     {
-        output.Append("</html>");
-        var outputPath = directory + Path.PathSeparator + "index.html";
-        File.WriteAllText(outputPath, output.ToString());
+        _reportContent.Append("</html>");
+        var outputPath = Path.Combine(_reportDirectory, "index.html");
+        File.WriteAllText(outputPath, _reportContent.ToString());
 
-        String cssPath = Configurator.Instance.GetPath("reportgeneratorcss");
-        var cssOutputPath = directory + Path.PathSeparator + "style.css";
+        var cssPath = Configurator.Instance.GetPath("reportgeneratorcss");
+        var cssOutputPath = Path.Combine(_reportDirectory, "style.css");
         File.Copy(cssPath, cssOutputPath);
     }
 
-    public void SaveImage(Bitmap bi, String filename)
+    /// <summary>
+    /// Saves an image to a file.
+    /// </summary>
+    /// <param name="image">The image to be saved.</param>
+    /// <param name="fileName">The name of the file where the image will be saved.</param>
+    public void SaveImage(Bitmap image, string fileName)
     {
-        String fileExtension = Path.GetExtension(filename);
-        if (!supportedFileFormats.Contains(fileExtension))
-        {
-            throw new InvalidOperationException("Unsupported file extension: " + fileExtension);
-        }
+        var fileExtension = Path.GetExtension(fileName);
 
-        var outputPath = directory + Path.PathSeparator + filename;
-        bi.Save(outputPath);
+        if (!SupportedImageFormats.Contains(fileExtension))
+            throw new InvalidOperationException("Unsupported file extension: " + fileExtension);
+
+        var outputPath = Path.Combine(_reportDirectory, fileName);
+        image.Save(outputPath);
     }
 }

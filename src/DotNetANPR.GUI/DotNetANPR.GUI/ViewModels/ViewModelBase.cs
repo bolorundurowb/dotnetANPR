e1842dee9f703e0f9664e2c470d2ebf69b890724
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-08-02 20:59:29 +0100

    ft: add in GUI project scaffold

---
+++ src/DotNetANPR.GUI/DotNetANPR.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,5 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DotNetANPR.GUI.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject { }
---
+++ src/DotNetANPR.GUI/DotNetANPR.GUI/Views/MainView.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace DotNetANPR.GUI.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView() { InitializeComponent(); }
+}
